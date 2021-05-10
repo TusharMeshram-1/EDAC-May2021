bc89976cf3ddb39c269f730386a9f0b010f2963e
TUSHAR MESHRAM_JH <83824283+TusharMeshram-1@users.noreply.github.com>
2021-05-11 00:56:27 +0530

    Add files via upload

---
+++ Assignment-2/Que22.java
@@ -0,0 +1,32 @@
+//22. Pattern-22
+/*  
+ 
+ *****
+  ****
+   ***
+    **
+     *
+
+*/
+//Code:
+
+public class Que22  
+{  
+	public static void main(String args[])   
+	{   
+		for(int i=1; i<=5; i++)  
+		{   
+			for(int j=1; j<=i; j++)  
+			{   
+				System.out.print(" ");   
+			} 
+			for(int k=5; k>=i; k--) 
+			{
+				System.out.print("*");   
+			}
+			System.out.println();      
+		}   
+	}   
+}  
+
+
