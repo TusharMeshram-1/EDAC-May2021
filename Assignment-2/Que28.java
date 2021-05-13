16eb994d47cf4658f3b0239069290093cc197b13
TUSHAR MESHRAM_JH <83824283+TusharMeshram-1@users.noreply.github.com>
2021-05-13 11:05:39 +0530

    Add files via upload

---
+++ Assignment-2/Que28.java
@@ -0,0 +1,34 @@
+//28. Pattern-28
+/*  
+ 
+     *****
+    *****
+   *****
+  *****
+ *****
+	 
+	 
+*/
+//Code:
+
+public class Que28  
+{  
+	public static void main(String args[])   
+	{   
+		for(int i=1; i<=5; i++)  
+		{   
+			for(int j=5; j>=i; j--)  
+			{   
+				System.out.print(" ");   
+			} 
+			for(int k=1; k<=5; k++)
+			{
+			System.out.print("*");
+		    }
+			
+			System.out.println();      
+		}   
+	}   
+}  
+
+
