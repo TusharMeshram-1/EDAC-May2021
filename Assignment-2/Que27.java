19b3e5098195d05ff81f2d4e3ecc0de6f8bb4f19
TUSHAR MESHRAM_JH <83824283+TusharMeshram-1@users.noreply.github.com>
2021-05-13 10:51:38 +0530

    Add files via upload

---
+++ Assignment-2/Que27.java
@@ -0,0 +1,51 @@
+//27. Pattern-27
+/*  
+ 
+     *
+    **
+   ***
+  ****
+ *****
+  ****
+   ***
+    **
+     *
+	 
+*/
+//Code:
+
+public class Que27  
+{  
+	public static void main(String args[])   
+	{   
+		for(int i=1; i<=5; i++)  
+		{   
+			for(int j=5; j>=i; j--)  
+			{   
+				System.out.print(" ");   
+			} 
+			for(int k=1; k<=i; k++)
+			{
+			System.out.print("*");
+		    }
+			
+			System.out.println();
+		}
+			for(int i=1; i<=4; i++) 
+			{
+				System.out.print(" ");
+				for(int j=1; j<=i; j++)
+				{
+				System.out.print(" ");    
+			    }
+				for(int k=4;k>=i;k--)
+				{
+					System.out.print("*");
+				}
+				
+			    System.out.println();      
+		    }   
+	}   
+}  
+
+
