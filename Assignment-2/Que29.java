b4197c9cbb7418570f29ae45e74e0317f882c860
TUSHAR MESHRAM_JH <83824283+TusharMeshram-1@users.noreply.github.com>
2021-05-13 11:11:18 +0530

    Add files via upload

---
+++ Assignment-2/Que29.java
@@ -0,0 +1,34 @@
+//29. Pattern-29
+/*  
+ 
+ *****
+  *****
+   *****
+    *****
+     *****
+	 
+	 
+*/
+//Code:
+
+public class Que29  
+{  
+	public static void main(String args[])   
+	{   
+		for(int i=1; i<=5; i++)  
+		{   
+			for(int j=1; j<=i; j++)  
+			{   
+				System.out.print(" ");   
+			} 
+			for(int k=1; k<=5; k++)
+			{
+			System.out.print("*");
+		    }
+			
+			System.out.println();      
+		}   
+	}   
+}  
+
+
